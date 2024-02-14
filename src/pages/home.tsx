700fc2598096c4e03e8c62b05c4680c8a7940b3c
ashik-75 <ashikex49@gmail.com>
2024-02-14 15:25:21 +0600

    fix:add strict mode

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,52 +1,5 @@
-import { Button } from "@/components/ui/button";
-import useFetch from "@/hooks/useFetch";
-import { Root } from "@/lib/types";
-import { Loader } from "lucide-react";
-import { useState } from "react";
-
 const Home = () => {
-	const [page, setPage] = useState(1);
-	const { isLoading, isError, data, error, refetch } = useFetch<Root>({
-		url: `https://rickandmortyapi.com/api/character/?page=${page}`,
-	});
-
-	if (isLoading) {
-		return (
-			<div>
-				<Loader size={30} className="animate-spin" />
-			</div>
-		);
-	}
-
-	if (isError && error) {
-		return <div>{error.message}</div>;
-	}
-	return (
-		<div className="space-y-5">
-			<div className="flex gap-5">
-				<Button onClick={refetch}>Refetch</Button>
-				<Button disabled={page === 1} onClick={() => setPage((p) => p - 1)}>
-					Prev
-				</Button>
-				<Button
-					disabled={page === data?.info.pages}
-					onClick={() => setPage((p) => p + 1)}
-				>
-					Next
-				</Button>
-			</div>
-			<br />
-
-			<div className="grid grid-cols-4 gap-5">
-				{data?.results.map((item) => (
-					<div>
-						<img src={item.image} alt={item.name} />
-					</div>
-				))}
-			</div>
-			<br />
-		</div>
-	);
+	return <div>Home</div>;
 };
 
 export default Home;
