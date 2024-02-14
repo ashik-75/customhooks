69571e25f979e0e2de2fe0e2e3fa3728a0d328e2
ashik-75 <ashikex49@gmail.com>
2024-02-14 18:59:27 +0600

    feat:remove options

--- src/pages/location.tsx
+++ src/pages/location.tsx
@@ -1,9 +1,8 @@
 import useLocation from "@/hooks/useLocation";
 import { format } from "date-fns";
-import { Send } from "lucide-react";
 
 const Location = () => {
-	const { coords, locatedAt, error } = useLocation({
+	const { coords, error } = useLocation({
 		enableHighAccuracy: true,
 		maximumAge: 1000,
 		timeout: 2000,
@@ -16,29 +15,36 @@ const Location = () => {
 		<div className="h-full flex items-center justify-center">
 			<div className="w-[350px] p-5 rounded-3xl space-y-1 border-pink-400 border">
 				<div className="flex justify-between">
-					<span className="uppercase font-medium text-sm">latitude</span>{" "}
+					<span className="uppercase font-medium text-sm text-zinc-500">
+						latitude
+					</span>{" "}
 					<span>{coords.latitude}</span>
 				</div>
 				<div className="flex justify-between">
-					<span className="uppercase font-medium text-sm">Longitude</span>{" "}
+					<span className="uppercase font-medium text-sm text-zinc-500">
+						Longitude
+					</span>{" "}
 					<span>{coords.longitude}</span>
 				</div>
 
 				<div className="flex justify-between">
-					<span className="uppercase font-medium text-sm">Speed</span>{" "}
+					<span className="uppercase font-medium text-sm text-zinc-500">
+						Speed
+					</span>{" "}
 					<span>{coords.speed}</span>
 				</div>
 
 				<div className="flex justify-between">
-					<span className="uppercase font-medium text-sm">Headed</span>{" "}
+					<span className="uppercase font-medium text-sm text-zinc-500">
+						Headed
+					</span>{" "}
 					<span>{coords.heading}</span>
 				</div>
 
-				<div className="flex gap-2">
-					<Send className="text-teal-400" /> <span>{locatedAt}</span>
-				</div>
 				<div className="flex justify-between">
-					<span className="uppercase font-medium text-sm">Updated At</span>{" "}
+					<span className="uppercase font-medium text-sm text-zinc-500">
+						Updated At
+					</span>{" "}
 					<span>{format(new Date(), "hh:mm:ss aa")}</span>
 				</div>
 			</div>
